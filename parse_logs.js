705242f4076c49f65c731f6d3a8840f2e39ced9a
Morgan Estes <morgan.estes@gmail.com>
2015-09-15 21:02:39 -0500

    Add 'changesets' argument for a shorter command
    With 'changesets', a user can pass a shorter argument on the command line
    to cut out ambiguity.
    Usage: `parse_logs.js --changesets 100:142` or `--changesets 100-142`.

--- parse_logs.js
+++ parse_logs.js
@@ -175,6 +175,22 @@ var logPath, logHTML,
 	stopRevision = parseInt(args['stop'], 10),
 	revisionLimit = parseInt(args['limit'], 10);
 
+	if (args['changesets']) {
+		var revisions = args['changesets'],
+			regex = /(\d{3,5})[:-](\d{3,5})/g,
+			matches;
+
+		while ((matches = regex.exec(revisions)) !== null) {
+			if (matches.index === regex.lastIndex) {
+				regex.lastIndex++;
+			}
+			console.dir(matches);
+
+			startRevision = parseInt(matches[2], 10);
+			stopRevision = parseInt(matches[1], 10);
+		}
+	}
+
 if ( isNaN(startRevision) || isNaN(stopRevision) ) {
 	console.log( "Usage: node parse_logs.js --start=<start_revision> --stop=<revision_to_stop> [--limit=<total_revisions>]\n" );
 	return;
